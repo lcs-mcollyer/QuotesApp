d0ba7555e4be4d384c783c6154798677d0b2f6a3
matt collyer <mcollyer@lcs.on.ca>
2022-02-22 09:48:12 -0500

    Initial Commit

---
+++ QuotesApp/QuotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesAppApp.swift
+//  QuotesApp
+//
+//  Created by Matthew Collyer on 2022-02-22.
+//
+
+import SwiftUI
+
+@main
+struct QuotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
